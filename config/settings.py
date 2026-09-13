3bc979f54fb797e488093975b205d014d31e093c
Mustafa Tahir <saimtahir113@gmail.com>
2026-09-13 01:32:11 -0700

    Fix PythonAnywhere allowed host

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "192.168.100.134",
     "172.17.242.116",
+    "mustafa113.pythonanywhere.com",
 ]
 
 if os.environ.get("RENDER_EXTERNAL_HOSTNAME"):
@@ -152,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
